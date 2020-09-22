2e0d03b1027252051256049216c7ca0be0cc89cf
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-22 09:04:05 +0100

    Added components to main

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import About from "./Sections/About";
+import Contact from "./Sections/Contact";
+import Projects from "./Sections/Projects";
+import Skills from "./Sections/Skills";
 
 export default function App() {
   return (
     <div className="app" style={{ position: "relative" }}>
       <Navbar />
       <Hero />
+      <Projects />
+      <Skills />
+      <About />
+      <Contact />
     </div>
   );
 }

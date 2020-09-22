c2f5fdbcbb165c64b3833291781430c085dd0f26
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-22 14:16:15 +0100

    Finished About section

--- src/Components/Sections/About.js
+++ src/Components/Sections/About.js
@@ -9,6 +9,18 @@ import {
 
 import { Container, SectionWrapper } from "../styled/Container";
 
+//Data
+const text =
+  "Hi! My name is Gracjan. \n\nI am a front-end web developer based in Manchester, UK.  \nFor development process I use React styled by SCSS or Styled Components. \nMy main focus is on writing good, maintainable code in accordance with DRY principles to maximize reusability and readability. \nI never overlook accessibility, responsiveness, performance as they are key to well developed website.";
+//Styles
+const SectionAboutContent = styled.section``;
+
+const SectionAboutContentText = styled.p`
+  font-weight: 300;
+  font-size: 3rem;
+  line-height: 1.5;
+  color: white;
+`;
 export default function About() {
   return (
     <SectionWrapper padded primary>
@@ -18,6 +30,19 @@ export default function About() {
             <SectionTitle>About</SectionTitle>
             <SectionSubtitle>Few words about me</SectionSubtitle>
           </SectionHeader>
+          <SectionAboutContent>
+            <SectionAboutContentText>
+              Hi! My name is Gracjan. <br />
+              <br />I am a front-end web developer based in Manchester, UK.
+              <br />
+              For development process I use React styled by SCSS or Styled
+              Components. <br />
+              My main focus is on writing good, maintainable code in accordance
+              with DRY principles to maximize reusability and readability.
+              <br />I never overlook accessibility, responsiveness, performance
+              as they are key to well developed website.
+            </SectionAboutContentText>
+          </SectionAboutContent>
         </StyledSectionContent>
       </Container>
     </SectionWrapper>
